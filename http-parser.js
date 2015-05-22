1022caf0a059f6b8d3f7d835db59972317893b42
Greg Richardson <gregnr@uvic.ca>
2015-05-22 15:37:00 -0700

    Split parser in to RequestParser and ResponseParser. Implemented RequestParser. Changed parsers to support pipelined HTTP requests (restarts state to GET_FIRST_LINE after parsing is complete)

--- http-parser.js
+++ http-parser.js
@@ -2,7 +2,7 @@ var events = require("events");
 var zlib = require("zlib");
 
 var State = {
-    GET_STATUS_CODE: 1,
+    GET_FIRST_LINE: 1,
     GET_HEADERS: 2,
     GET_BODY: 3,
     PARSE_COMPLETE : 4
@@ -34,7 +34,7 @@ var bufferIndexOf = function(buffer, searchString) {
     return -1;
 };
 
-var HTTPParser = function() {
+var RequestParser = function() {
 
     //Init:
 
@@ -51,8 +51,181 @@ var HTTPParser = function() {
         headers: []
     };
     
-    //The start state will be looking for an HTTP status code
-    var state = State.GET_STATUS_CODE;
+    //The start state will be looking for the first line
+    var state = State.GET_FIRST_LINE;
+    
+    //Buffer to store incoming data chunks
+    var binaryBuffer = new Buffer(0);
+    
+    //Private functions:
+    
+    var getFirstLine = function() {
+        
+        var firstLineEnd = bufferIndexOf(binaryBuffer, "\r\n");
+        
+        //Return if we do not have a complete first line
+        if (firstLineEnd === -1) return false;
+        
+        var firstLine = binaryBuffer.toString("utf8", 0, firstLineEnd);
+        
+        //Get HTTP Method, URL, and version from first line
+        var httpRegex = /(.+) (.+) HTTP\/(.+)/;
+        var httpMatch = firstLine.match(httpRegex);
+        
+        //Return if we do not have a match
+        if (!httpMatch) return false;
+        
+        httpObject.method = httpMatch[1];
+        httpObject.url = httpMatch[2];
+        httpObject.version = httpMatch[3];
+        
+        //Remove the data we parsed from the buffer
+        binaryBuffer = binaryBuffer.slice(firstLineEnd + Buffer.byteLength("\r\n"));
+        
+        return true;
+    };
+    
+    var getHeaders = function() {
+        
+        var headerEnd = bufferIndexOf(binaryBuffer, "\r\n\r\n");
+        
+        //Return if we do not have a complete header
+        if (headerEnd === -1) return false;
+    
+        //rawHeader includes everything up until the headerEnd
+        var rawHeader = binaryBuffer.toString("utf8", 0, headerEnd);
+        
+        var headerEntries = rawHeader.split("\r\n");
+        
+        for (var i = 0; i < headerEntries.length; i++) {
+            
+            var headerEntry = headerEntries[i].toLowerCase();
+            var headerKeyValue = headerEntry.split(": ");
+            
+            if (headerKeyValue.length > 1) {
+                
+                //Get content length if it's set
+                if (headerKeyValue[0] === "content-length") {
+                    httpObject.contentLength = headerKeyValue[1];
+                }
+                
+                httpObject.headers.push(headerKeyValue);
+            }
+        }
+        
+        //Remove the data we parsed from the buffer
+        binaryBuffer = binaryBuffer.slice(headerEnd + Buffer.byteLength("\r\n\r\n"));
+        
+        return true;
+    };
+    
+    var getBody = function() {
+    
+        if (httpObject.contentLength && httpObject.contentLength > 0) {
+            
+            var contentLength = parseInt(httpObject.contentLength);
+            
+            //Return if we do not have a complete body
+            if (binaryBuffer.length < contentLength) return false;
+            
+            //Create a new buffer to store the body
+            var body = new Buffer(contentLength);
+            binaryBuffer.copy(body);
+            
+            httpObject.body = body;
+            
+            //Remove the data we parsed from the buffer
+            binaryBuffer = binaryBuffer.slice(contentLength + Buffer.byteLength("\r\n"));
+            
+            return true;
+            
+        } else {
+        
+            //The HTTP response has no body
+            //We may need to strip off some newlines from the buffer...?
+            httpObject.body = new Buffer(0);
+            
+            return true;
+        }
+    };
+    
+    var executeState = function() {
+    
+        switch (state) {
+        
+            case State.GET_FIRST_LINE:
+                
+                if (getFirstLine()) {
+                
+                    state = State.GET_HEADERS;
+                    executeState();
+                }
+                break;
+                
+            case State.GET_HEADERS:
+            
+                if (getHeaders()) {
+        
+                    that.emit("headersLoaded", httpObject.headers);
+                    
+                    state = State.GET_BODY;
+                    executeState();
+                }
+                break;
+                
+            case State.GET_BODY:
+            
+                if (getBody()) {
+                    
+                    that.emit("bodyLoaded", httpObject.body);
+                    that.emit("parseComplete", httpObject);
+                    
+                    state = State.GET_FIRST_LINE;
+                    
+                    //Create a new httpObject for the next pipelined request
+                    httpObject = {
+                        headers: []
+                    };
+                    
+                    executeState();
+                }
+                break;
+        }
+        
+        //console.log("Request State:", state);
+    };
+    
+    
+    //Public functions:
+    
+    this.pushChunk = function(chunk) {
+        
+        //TODO: Make this more memory efficient (don't create a new buffer every time a chunk comes in)
+        binaryBuffer = Buffer.concat([binaryBuffer, chunk]);
+        
+        executeState();
+    };
+};
+
+var ResponseParser = function() {
+
+    //Init:
+
+    //Call the EventEmitter constructor
+    events.EventEmitter.call(this);
+    
+    var that = this;
+    
+    
+    //Private variables:
+    
+    //HTTP object that we will build
+    var httpObject = {
+        headers: []
+    };
+    
+    //The start state will be looking for the first line
+    var state = State.GET_FIRST_LINE;
     
     //Buffer to store incoming data chunks
     var binaryBuffer = new Buffer(0);
@@ -63,7 +236,7 @@ var HTTPParser = function() {
     
     //Private functions:
     
-    var getStatusCode = function() {
+    var getFirstLine = function() {
         
         var firstLineEnd = bufferIndexOf(binaryBuffer, "\r\n");
         
@@ -134,7 +307,7 @@ var HTTPParser = function() {
     
     var getBody = function() {
         
-        if (httpObject.transferEncoding === "chunked") {
+        if (httpObject.transferEncoding && httpObject.transferEncoding === "chunked") {
             
             var firstLineEnd = bufferIndexOf(binaryBuffer, "\r\n");
             
@@ -186,7 +359,8 @@ var HTTPParser = function() {
             
             httpObject.body = body;
             
-            //TODO: Remove parsed data
+            //Remove the data we parsed from the buffer
+            binaryBuffer = binaryBuffer.slice(contentLength + Buffer.byteLength("\r\n"));
             
             return true;
             
@@ -194,6 +368,8 @@ var HTTPParser = function() {
         
             //The HTTP response has no body (no content length and is not chunked)
             //We may need to strip off some newlines from the buffer...?
+            httpObject.body = new Buffer(0);
+            
             return true;
         }
     };
@@ -202,9 +378,9 @@ var HTTPParser = function() {
     
         switch (state) {
         
-            case State.GET_STATUS_CODE:
+            case State.GET_FIRST_LINE:
                 
-                if (getStatusCode()) {
+                if (getFirstLine()) {
                 
                     state = State.GET_HEADERS;
                     executeState();
@@ -227,26 +403,38 @@ var HTTPParser = function() {
                 if (getBody()) {
                     
                     //Decompress gzipped bodies
-                    if (httpObject.contentEncoding.indexOf("gzip") !== -1) {
-                        
-                        console.log("We're gzipped yo");
+                    if (httpObject.contentEncoding && httpObject.contentEncoding.indexOf("gzip") !== -1) {
                         
                         //Store the raw (compressed) body in rawBody
                         httpObject.rawBody = httpObject.body;
                         
                         //Inflate (decompress) the body
-                        httpObject.body = zlib.unzipSync(httpObject.rawBody);
+                        try {
+                        
+                            httpObject.body = zlib.unzipSync(httpObject.rawBody);
+                            
+                        } catch(error) {
+                        
+                            that.emit("error", error);
+                        }
                     }
                     
                     that.emit("bodyLoaded", httpObject.body);
                     that.emit("parseComplete", httpObject);
                     
-                    state = State.PARSE_COMPLETE;
+                    state = State.GET_FIRST_LINE;
+                    
+                    //Create a new httpObject for the next pipelined request
+                    httpObject = {
+                        headers: []
+                    };
+                    
+                    executeState();
                 }
                 break;
         }
         
-        console.log("State:", state);
+        //console.log("Response State:", state);
     };
     
     
@@ -261,7 +449,11 @@ var HTTPParser = function() {
     };
 };
 
-//Make HTTPParser an event emitter
-HTTPParser.prototype.__proto__ = events.EventEmitter.prototype;
+//Make RequestParser an event emitter
+RequestParser.prototype.__proto__ = events.EventEmitter.prototype;
 
-module.exports.HTTPParser = HTTPParser;
+//Make ResponseParser an event emitter
+ResponseParser.prototype.__proto__ = events.EventEmitter.prototype;
+
+module.exports.RequestParser = RequestParser;
+module.exports.ResponseParser = ResponseParser;
